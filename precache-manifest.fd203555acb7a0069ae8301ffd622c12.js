c0661b22a91c6668e770ca733cf43b17befe6f2d
Julian Atkin <jmd.atkin@gmail.com>
2020-07-23 18:57:30 +0100

    Updates

---
+++ precache-manifest.fd203555acb7a0069ae8301ffd622c12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c5364c68178bfd28a8c5cc837ad581",
+    "url": "/fastlist/index.html"
+  },
+  {
+    "revision": "4e3304b02436e1c9c3b6",
+    "url": "/fastlist/static/css/main.da4070d6.chunk.css"
+  },
+  {
+    "revision": "44a2f9753465d6631a94",
+    "url": "/fastlist/static/js/2.bccbb1fa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fastlist/static/js/2.bccbb1fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e3304b02436e1c9c3b6",
+    "url": "/fastlist/static/js/main.8626e357.chunk.js"
+  },
+  {
+    "revision": "fcb9b37b5a89951b3231",
+    "url": "/fastlist/static/js/runtime-main.f4a155be.js"
+  }
+]);
