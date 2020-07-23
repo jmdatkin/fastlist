7be9b301e21737c32befd0841b671e4be9433f42
Julian Atkin <jmd.atkin@gmail.com>
2020-07-23 05:46:09 +0100

    init repo

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppWrapper from '../AppWrapper/AppWrapper.js';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppWrapper />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/EntryContent/EntryContent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function EntryContent(props) {
+    return (
+        <span className="EntryContent">{props.content}</span>
+    );
+}
+
+export default EntryContent;
