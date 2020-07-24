8c415d0041b25745d0b4741bcfc9565eccc3802b
Julian Atkin <jmd.atkin@gmail.com>
2020-07-24 05:12:05 +0100

    updated new dev branch with changes

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import BrowserRouter from 'react-router-dom';
-import AppWrapper from '../AppWrapper/AppWrapper.js';
+import AppWrapper from './AppWrapper/AppWrapper.js';
 import './App.css';
 
 
--- src/EntryContent/EntryContent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function EntryContent(props) {
-    return (
-        <span className="EntryContent">{props.content}</span>
-    );
-}
-
-export default EntryContent;
